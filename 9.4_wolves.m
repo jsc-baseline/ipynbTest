b31d32fcf8ca590804bc4ffe88f17a9054838a29
jsc-baseline <15642207+jsc-baseline@users.noreply.github.com>
2018-11-08 09:26:50 -0500

    Update 9.4_wolves.m

--- 9.4_wolves.m
+++ 9.4_wolves.m
@@ -4,6 +4,7 @@ birth = 0.065;              % natural birth rate
 death = 0.032;              % natural death rate 
 bacterial_infection = 0.70; % reduces population 
 deltat = 1;                 % time step: year
+%{
 for t = 1:21                                                    % loops 21-year period
     wolf(t + 1) = wolf(t) + deltat * (birth - death) * wolf(t); % computes population growth
     if (mod(t, 2) != 0 && t > 1)                                % checks if year is odd
@@ -14,6 +15,19 @@ for t = 1:21                                                    % loops 21-year 
     end                                                         % ends check
     wolf(t + 1) = floor(wolf(t + 1));                           % makes whole wolf 
 end                                                             % ends loop 
-wolf(22)         % at beginning of year 22 
-% plot(1:22, wolf) % plot of rabbit population as function of time for 75-year period 
- 
+fprintf("At beginning of year 22: %d wolves\n", wolf(22))       % prints result
+plot(1:22, wolf) % plot of wolf population as function of time for 21-year period 
+%}
+
+for t = 1:21                                                    % loops 21-year period
+    wolf(t + 1) = wolf(t) + deltat * (birth - death) * wolf(t); % computes population growth
+    if (mod(t, 2) != 0 && t > 1)                                % checks if year is odd
+        wolf(t + 1) = wolf(t + 1) + 22;                         % wolves from neighboring Idaho 
+    end                                                         % ends check
+    if (mod(t, 6) == 0)                                         % checks if year is sixth
+	wolf(t + 1) = wolf(t + 1) * (1 - bacterial_infection);      % computes population after outbreak
+    end                                                         % ends check
+    wolf(t + 1) = floor(wolf(t + 1));                           % makes whole wolf 
+end                                                             % ends loop 
+fprintf("At beginning of year 22: %d wolves\n", wolf(22))       % prints result
+plot(1:22, wolf) % plot of wolf population as function of time for 21-year period
